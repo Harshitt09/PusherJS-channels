a2b7d44b31478973678da460c347aa5976f9b154
Harshitt09 <harshitt09@gmail.com>
2019-11-05 22:15:10 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://localhost:27017/pusher',
+  pusherAppId: '890677',
+  pusherKey: 'a07181b7a0218935a12f',
+  pusherSecret: 'e92096001d59a27839e7',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
